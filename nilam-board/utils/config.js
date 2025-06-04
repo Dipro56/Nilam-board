89b6d64997cfffdd1400df80a2a72c9ac3f44310
Sadat Shahriar <sadat@gakkmediabd.com>
2025-06-04 16:45:19 +0600

    base url updated

--- nilam-board/utils/config.js
+++ nilam-board/utils/config.js
@@ -1,1 +1,3 @@
-export const baseUrl = "http://localhost:8002/api/v1";
+// export const baseUrl = "http://localhost:8002/api/v1";
+
+export const baseUrl = "https://nilam-board-backend.onrender.com/api/v1";
