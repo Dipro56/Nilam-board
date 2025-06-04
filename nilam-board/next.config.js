56dbf81ef05260366b7ef24b696ddfbd94e97279
Sadat Shahriar Bari <shahriardipro38@gmail.com>
2025-06-05 04:47:55 +0600

    next option updated

--- nilam-board/next.config.js
+++ nilam-board/next.config.js
@@ -3,10 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "localhost",
-      "res.cloudinary.com",
-      "ratings-images-prod.pulse.ea.com",
+      'localhost',
+      'res.cloudinary.com',
+      'ratings-images-prod.pulse.ea.com',
     ],
+  },
+  compilerOptions: {
+    forceConsistentCasingInFileNames: true,
   },
 };
 
