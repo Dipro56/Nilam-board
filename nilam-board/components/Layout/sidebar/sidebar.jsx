c735eab6a4b8c7c621863d050ccc55b0995fde3f
Sadat Shahriar Bari <shahriardipro38@gmail.com>
2025-06-15 19:26:21 +0600

    updated side bar

--- nilam-board/components/Layout/sidebar/sidebar.jsx
+++ nilam-board/components/Layout/sidebar/sidebar.jsx
@@ -208,7 +208,7 @@ const Sidebar = () => {
             <div className="w-full">
           <Menu as="div" className="relative inline-block text-left">
             <div>
-              <Menu.Button className="text-white bg-white text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center   ">
+              <Menu.Button className=" bg-white text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center   ">
                 {playerPosition ? playerPosition : 'See Manger Details'}
                 <svg
                   class="w-4 h-4 ml-2"
